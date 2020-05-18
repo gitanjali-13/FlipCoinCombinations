8fd76ba954defbb0557c445b7199019b948008b4
gitanjali-13 <gitaanjalide@gmail.com>
2020-05-18 12:57:18 +0530

    Triplet combinations

--- flipCoinCombo.sh
+++ flipCoinCombo.sh
@@ -1,37 +1,56 @@
 #!/bin/bash -x
-declare -A DictDouble
+declare -A DoubleDict
 declare -A PercentDouble
-hh=0
-tt=0
-th=0
-ht=0
+hhh=0
+hht=0
+thh=0
+hth=0
+ttt=0
+tth=0
+htt=0
+tht=0
 
 for ((count=0; count<20; count++))
 do
-	result=$((RANDOM % 4))
+	result=$((RANDOM % 8))
 	if [ $result -eq 0 ]
 	then
-		$((hh++))
+		$((hhh++))
 	elif [ $result -eq 1 ]
 	then
-		$((ht++))
+		$((hht++))
 	elif [ $result -eq 2 ]
 	then
-		$((th++))
-	elif [ $result -eq 3 ]
+		$((thh++))
+        elif [ $result -eq 3 ]
+        then
+                $((hth++))
+        elif [ $result -eq 4 ]
+        then
+                $((ttt++))
+        elif [ $result -eq 5 ]
+        then
+                $((tth++))
+        elif [ $result -eq 6 ]
+        then
+                $((htt++))
+	elif [ $result -eq 7 ]
 	then
-		$((tt++))
+		$((tht++))
 	fi
 done
 
-DictDouble[HH]=$hh
-DictDouble[TT]=$tt
-DictDouble[HT]=$ht
-DictDouble[TH]=$th
+DoubleDict[HHH]=$hhh
+DoubleDict[TTT]=$ttt
+DoubleDict[HHT]=$hht
+DoubleDict[TTH]=$tth
+DoubleDict[THH]=$thh
+DoubleDict[HTT]=$htt
+DoubleDict[HTH]=$hth
+DoubleDict[THT]=$tht
 
-for i in ${!DictDouble[@]}
+for i in ${!DoubleDict[@]}
 do
-	PercentDouble[$i]=$((${DictDouble[$i]} * 100 / 20))
+	PercentDouble[$i]=$((${DoubleDict[$i]} * 100 / 20))
 done
 echo ${PercentDouble[@]}
-
